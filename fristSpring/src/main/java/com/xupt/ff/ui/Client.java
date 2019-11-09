bf40c94e08aa6e474667ef9e6bf2c55bcf2e24e4
cc_ff <909278799@qq.com>
2019-11-10 02:24:08 +0800

    0

--- fristSpring/src/main/java/com/xupt/ff/ui/Client.java
+++ fristSpring/src/main/java/com/xupt/ff/ui/Client.java
@@ -8,9 +8,26 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 /**
  * @author cc_ff
  * @create 2019-11-10 上午1:03
- * @action
+ * @action 模拟一个表现层，用于调用业务
  */
 public class Client {
+
+    /**
+     * 获取spring的Ioc核心容器，并根据id获取对象
+     * ApplicationContext的三个常用实现类
+     *      ClassPathXmlApplicationContext :它可以加载类路径下的配置文件，要求配置文件必须在类路径下
+     *      FileSystemXmlApplicationContext :它可以加载磁盘任意路径下的配置文件，（必须有访问权限）
+     *      AnnotationConfigApplicationContext :它可以用于读取注解创建容器的。
+     *
+     * 核心容器的两个接口引发出的问题：
+     *      ApplicationContext： 单例对象适用
+     *          它在构建核心容器时，创建对象是采取立即加载的方式
+     *
+     *      BeanFactory：    多例对象适用
+     *          它在构建核心容器时，创建对象是采取延迟加载的方式
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         //1.获取核心容器对象
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
